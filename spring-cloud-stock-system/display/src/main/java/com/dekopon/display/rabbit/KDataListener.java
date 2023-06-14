f54e76b2dd44706ef17a448b0719a09a20ef4ea3
uu60 <74050611+uu60@users.noreply.github.com>
2023-06-14 16:39:24 +0800

    [add]complete data query

--- spring-cloud-stock-system/display/src/main/java/com/dekopon/display/rabbit/KDataListener.java
+++ spring-cloud-stock-system/display/src/main/java/com/dekopon/display/rabbit/KDataListener.java
@@ -2,8 +2,10 @@ package com.dekopon.display.rabbit;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.dekopon.display.configuration.RabbitConfiguration;
+import com.dekopon.display.configuration.RedisConfiguration;
 import com.dekopon.display.dao.KDataMapper;
 import com.dekopon.display.entity.KDataEntity;
+import com.dekopon.display.service.KDataService;
 import com.dekopon.pojo.R;
 import com.google.gson.Gson;
 import com.rabbitmq.client.Channel;
@@ -11,6 +13,7 @@ import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,7 +36,11 @@ public class KDataListener {
     @Autowired
     KDataMapper kDataMapper;
     @Autowired
+    KDataService kDataService;
+    @Autowired
     Gson gson;
+    @Autowired
+    StringRedisTemplate redisTemplate;
 
     @RabbitListener(queues = RabbitConfiguration.K_DATA_RESULT_QUEUE_NAME)
     public void saveToDB(Message message,
@@ -41,8 +48,7 @@ public class KDataListener {
                          Channel channel) {
         String code = content.getMsg();
         // 获取加载过的最新的记录
-        KDataEntity kDataEntity = kDataMapper.selectOne(new LambdaQueryWrapper<KDataEntity>().eq(KDataEntity::getCode
-                , code).orderByDesc(KDataEntity::getTime).last("limit 1"));
+        KDataEntity kDataEntity = kDataService.getLatestKData(code);
 
         List<KDataEntity> records = ((List<Map<String, Object>>) content.getData()).stream().map(map -> {
             KDataEntity entity = new KDataEntity();
@@ -63,7 +69,7 @@ public class KDataListener {
             records = records.stream().filter(record -> record.getTime().compareTo(kDataEntity.getTime()) > 0).toList();
         }
         records.forEach(e -> kDataMapper.insert(e));
-
+        redisTemplate.opsForValue().getAndDelete(RedisConfiguration.K_DATA_PREFIX + code);
 
         try {
             channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
--- spring-cloud-stock-system/display/src/main/java/com/dekopon/display/service/impl/KDataServiceImpl.java
+++ spring-cloud-stock-system/display/src/main/java/com/dekopon/display/service/impl/KDataServiceImpl.java
@@ -1,8 +1,8 @@
 package com.dekopon.display.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.dekopon.display.configuration.RabbitConfiguration;
+import com.dekopon.display.configuration.RedisConfiguration;
 import com.dekopon.display.dao.KDataMapper;
 import com.dekopon.display.entity.KDataEntity;
 import com.dekopon.display.service.KDataService;
@@ -15,6 +15,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -36,12 +37,10 @@ public class KDataServiceImpl implements KDataService {
     @Autowired
     ThreadPoolExecutor pool;
 
-    final static String REDIS_PREFIX = "k_data::";
-
     @Override
     public List<KDataEntity> getExistedData(String code) {
         // 先查询redis
-        String data = redisTemplate.opsForValue().get(REDIS_PREFIX + code);
+        String data = redisTemplate.opsForValue().get(RedisConfiguration.K_DATA_PREFIX + code);
         // 有则直接返回
         if (StringUtils.hasText(data)) {
             return gson.fromJson(data, new TypeToken<List<KDataEntity>>() {
@@ -54,21 +53,28 @@ public class KDataServiceImpl implements KDataService {
         // 如果有值，写入redis并返回
         // TODO: 防止缓存击穿
         if (!kDataEntities.isEmpty()) {
-            redisTemplate.opsForValue().set(REDIS_PREFIX + code, gson.toJson(kDataEntities), millisToNextHour(),
-                    TimeUnit.MILLISECONDS);
+            long current = System.currentTimeMillis();
+            redisTemplate.opsForValue().set(RedisConfiguration.K_DATA_PREFIX + code, gson.toJson(kDataEntities),
+                    nextHalfHourMillis(current) - current, TimeUnit.MILLISECONDS);
         }
         // 否则异步交给python取获取股票数据
-        else {
+        if (kDataEntities.isEmpty() || nextHalfHourMillis(kDataEntities.get(kDataEntities.size() - 1).getTime().getTime()) < System.currentTimeMillis()) {
             pool.execute(() -> {
                 rabbitTemplate.convertAndSend("", RabbitConfiguration.K_DATA_QUERY_QUEUE_NAME, R.other(10000));
             });
         }
+        Collections.reverse(kDataEntities);
+
         return kDataEntities;
     }
 
-    private long millisToNextHour() {
-        long currentStamp = System.currentTimeMillis();
-        long nextStamp = (currentStamp / (60 * 60 * 1000) + 1) * (60 * 60 * 1000);
-        return nextStamp - currentStamp;
+    @Override
+    public KDataEntity getLatestKData(String code) {
+        // 获取加载过的最新的记录
+        return kDataMapper.selectOne(new LambdaQueryWrapper<KDataEntity>().eq(KDataEntity::getCode, code).orderByDesc(KDataEntity::getTime).last("limit 1"));
+    }
+
+    private long nextHalfHourMillis(long timeStamp) {
+        return (timeStamp / (30 * 60 * 1000) + 1) * (30 * 60 * 1000);
     }
 }
