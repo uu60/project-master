25655ddf73012f721f276fd00c45f95aeb52827c
uu60 <74050611+uu60@users.noreply.github.com>
2023-07-02 16:24:04 +0800

    [optim]优化目录

---
+++ stock-backend-spring-cloud/prediction-app/src/main/java/com/dekopon/prediction/PredictionApplication.java
@@ -0,0 +1,13 @@
+package com.dekopon.prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictionApplication.class, args);
+	}
+
+}
