72a45e940de4aba2b981a33f2222b8c3bf519aa7
uu60 <74050611+uu60@users.noreply.github.com>
2023-07-02 16:24:04 +0800

    [optim]优化目录

---
+++ stock-backend-spring-cloud/prediction-app/src/test/java/com/dekopon/prediction/PredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dekopon.prediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
