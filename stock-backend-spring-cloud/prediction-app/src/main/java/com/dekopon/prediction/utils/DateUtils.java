c92c5ac884f55b78f4e83fec1790df2aabdbc3e0
uu60 <74050611+uu60@users.noreply.github.com>
2023-07-15 15:35:05 +0800

    [fix]score api

--- stock-backend-spring-cloud/prediction-app/src/main/java/com/dekopon/prediction/utils/DateUtils.java
+++ stock-backend-spring-cloud/prediction-app/src/main/java/com/dekopon/prediction/utils/DateUtils.java
@@ -1,7 +1,12 @@
 package com.dekopon.prediction.utils;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.Date;
 
 /**
  * @author dekopon
@@ -24,5 +29,11 @@ public class DateUtils {
         } catch (DateTimeParseException e) {
             return false;
         }
+    }
+
+    public static Date getStartOfDay(Date date) {
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(date.getTime()), ZoneId.systemDefault());
+        LocalDateTime startOfDay = localDateTime.with(LocalTime.MIN);
+        return Date.from(startOfDay.atZone(ZoneId.systemDefault()).toInstant());
     }
 }
